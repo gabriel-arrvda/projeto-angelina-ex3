3dd812aa02c7b79e07eb9a5a7d0c716bfcc94588
Gabriel Lucas Arruda <gabriellucas_arruda@hotmail.com>
2022-04-23 22:39:47 -0300

    feat: :white_check_mark: Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header'
+import { useState } from 'react'
+import {toast} from 'react-toastify' 
 
-function App() {
+export default function App() {
+
+  const [nome, setNome] = useState();
+  const [endereco, setEndereco] = useState();
+  const [cidade, setCidade] = useState();
+  const [estado, setEstado] = useState();
+  const [telefone, setTelefone] = useState();
+
+  function nomeAlterar(e) {
+    setNome(e.target.value);
+  }
+
+  function enderecoAlterar(e) {
+    setEndereco(e.target.value);
+  }
+
+  function cidadeAlterar(e) {
+    setCidade(e.target.value);
+  }
+
+  function estadoAlterar(e) {
+    setEstado(e.target.value);
+  }
+
+  function telefoneAlterar(e) {
+    setTelefone(e.target.value);
+  }
+
+  function enviarValidar(e) {
+    e.preventDefault()
+
+    if(!nome) 
+      toast.error("Erro. Nome não pode estar vazio") 
+    else 
+      toast.success("Dados Cadastrados! ") 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <form onSubmit={enviarValidar}>
+        <div className="formGroup">
+          <div class="row">
+            <input class='boxInput' type='text' placeholder='Nome' value={nome} onChange={nomeAlterar} ></input>
+            <input class='boxInput' type='text' placeholder='Telefone' value={telefone} onChange={telefoneAlterar} ></input>
+          </div>
+
+          <div class="row">
+            <input class='boxInput' type='text' placeholder='Endereço' value={endereco} onChange={enderecoAlterar} ></input>
+            <input class='boxInput' type='text' placeholder='Cidade' value={cidade} onChange={cidadeAlterar} ></input>
+            <input class='boxInput' type='text' placeholder='Estado' value={estado} onChange={estadoAlterar} ></input>
+          </div>
+        </div>
+
+        <input class='btnSend' type={'submit'}></input>
+      </form>
     </div>
   );
 }
 
-export default App;
+
+
+
+
